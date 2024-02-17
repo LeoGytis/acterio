f741717863f8c98b9f6419e6342e76e9b3dde6cd
leogytis <leogytis@gmail.com>
2024-02-17 16:47:48 +0200

    input edge cases

--- src/app/posts/components/SearchBar.tsx
+++ src/app/posts/components/SearchBar.tsx
@@ -9,6 +9,7 @@ interface SearchBarProps {
 
 const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
 	const [searchWord, setSearchWord] = useState("");
+	const [errorMessage, setErrorMessage] = useState(""); // New state for error message
 	const router = useRouter();
 
 	const pathname = usePathname();
@@ -19,6 +20,15 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
 	};
 
 	const handleSearch = () => {
+		if (searchWord.trim() === "") {
+			setErrorMessage("Please enter a search word.");
+			return;
+		}
+		if (searchWord.length > 16) {
+			setErrorMessage("Search term should not be longer than 16 characters.");
+			return;
+		}
+		setErrorMessage("");
 		if (onSearch) {
 			onSearch(searchWord);
 		}
@@ -28,7 +38,7 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
 		<nav className="sticky top-0 left-0 z-50 w-full h-16 bg-main shadow-lg">
 			<div className="mx-auto max-w-screen-xl h-full flex justify-between items-center text-xs px-10">
 				<Image src="/images/logo.svg" width={100} height={100} alt="logo" className="w-36 h-20" />
-
+				{errorMessage && <span className="text-base text-red-500">{errorMessage}</span>}
 				{isPostPage ? (
 					<div className="blur-animation">
 						<input
